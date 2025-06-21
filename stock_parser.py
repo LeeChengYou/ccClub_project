76a5ca81163ecb00d6bce9e24d24f547b2ad8626
LeeChengYou <901126jeff901126@gmail.com>
2025-06-21 21:49:33 +0800

    define download function to get csv files

--- stock_parser.py
+++ stock_parser.py
@@ -4,3 +4,11 @@ def get_stock_data(symbol, period="1mo"):
     stock = yf.Ticker(symbol)
     hist = stock.history(period=period)
     return hist
+def download_stock_data(symbol, period="1mo"):
+    stock = yf.Ticker(symbol)
+    hist = stock.history(period=period)
+    if hist.empty:
+        return 0
+    else:
+        yf.download(symbol, period=period, auto_adjust=True, progress=False)
+        return yf.Ticker(symbol).history(period=period) 
